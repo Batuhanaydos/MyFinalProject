427bd22430d4526b792e045ce1e4cb60ac6550b4
BATUHAN AYDOS <batuhanaydos12@gmail.com>
2023-08-22 16:12:08 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,12 +7,22 @@ namespace ConsoleUI
 {
     class Program
     {
+
+        //static void Main(string[] args)
+        //{
+        //    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        //    foreach (var category in categoryManager.GetAll())
+        //    {
+        //        Console.WriteLine(category.CategoryName);
+        //    }
+        //}
+
         static void Main(string[] args)
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            foreach (var product in productManager.GetProductDetials())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.Productname + " /" + product.CategoryName);
             }
         }
     }
